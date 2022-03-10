6751207ec118bc29544cbf0614344d197516be93
Bhanumaan <101312783+Bhanumaan@users.noreply.github.com>
2022-03-10 12:01:28 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,11 @@
+var=[1,2,3,4]
+var2=['a','b','c','d']
+print(var)
+print(var[1])  #index value
+
+print(var[-2])    #-1 will be the last
+
+fruits=['apple','banana','cherry']
+fruits.append('orange')  #append means add to last position
+print(fruits)
+
